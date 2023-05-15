c5edcef2019159cdbd6f914cb5fd48e4938d29dc
Your Name <you@example.com>
2023-05-15 15:54:06 +0800

    test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         // test github action 2
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!2" );
     }
 }
