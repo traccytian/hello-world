0fbefe49bad0dff9b3ef813eae34bb1a381555ec
selab722 <75660445+selab722@users.noreply.github.com>
2023-05-16 10:47:40 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         // test github action 2
-        System.out.println( "Hello World!222" );
+        System.out.println( "Hello World!2223" );
     }
 }
